43fc868bd584fcb6ee3461c5f6594e6c9bba9947
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-12-07 13:30:04 -0200

    Editando uma tarefa

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,27 @@
 class TasksController < ApplicationController
   def new
     @task = Task.new
+    @categories = Category.all
   end
 
   def create
-    @task = Task.new(params.require(:task).permit(:description, :status))
+    @task = Task.new(params.require(:task).permit(:description, :status, :category_id))
      if @task.save
        redirect_to root_path
-     else 
+     else
+      @categories = Category.all
       render :new
      end
   end
 
   def edit
     @task = Task.find(params[:id])
+    @categories = Category.all
   end 
 
   def update
     @task = Task.find(params[:id])
-    @task.update(params.require(:task).permit(:description, :status))
+    @task.update(params.require(:task).permit(:description, :status, :category_id))
     redirect_to root_path
   end
 
